f4dd33dd8d82f1d7089ec7300d8ba863e8c6e4e8
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-10-19 09:48:11 +0900

    05_track-mern

--- 05_track-mern/track-server/src/index.js
+++ 05_track-mern/track-server/src/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
 
+require('dotenv').config();
+require('./models/User');
+require('./models/Track');
+
+const requireAuth = require('./middlewares/requireAuth');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
 
 const app = express();
+app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 mongoose.connect(process.env.MONGO_URI);
 mongoose.connection.on('connected', () => {
@@ -15,8 +23,8 @@ mongoose.connection.on('error', (err) => {
   console.log(err);
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello');
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
--- 05_track-mern/track-server/src/routes/authRoutes.js
+++ 05_track-mern/track-server/src/routes/authRoutes.js
@@ -1,9 +1,47 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
+const User = mongoose.model('User');
 const router = express.Router();
 
-router.post('/signup', (req, res) => {
-  res.send('POST');
+router.post('/signup', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = new User({ email, password });
+    await user.save();
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+
+    res.send({ token });
+  } catch (err) {
+    res.status(422).send(err.message);
+  }
+});
+
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).send({ error: 'Invalid Credentials' });
+  }
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    return res.status(401).send({ error: 'Invalid Credentials' });
+  }
+
+  try {
+    await user.comparePassword(password);
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+
+    res.send({ token });
+  } catch (err) {
+    return res.status(401).send({ error: 'Invalid Credentials' });
+  }
 });
 
 module.exports = router;
